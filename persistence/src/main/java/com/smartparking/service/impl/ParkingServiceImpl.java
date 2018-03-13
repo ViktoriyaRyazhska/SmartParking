ee18ee6672b503b5146583bfa463f6da5ea0bd7f
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-03-13 17:05:54 +0200

    Fixed ParkingServiceImpl.

--- persistence/src/main/java/com/smartparking/service/impl/ParkingServiceImpl.java
+++ persistence/src/main/java/com/smartparking/service/impl/ParkingServiceImpl.java
@@ -1,16 +1,16 @@
 package com.smartparking.service.impl;
 
-import com.smartparking.entity.Favorite;
-import com.smartparking.repository.FavoriteRepository;
+import com.smartparking.entity.Parking;
+import com.smartparking.repository.ParkingRepository;
 import com.smartparking.service.AbstractService;
-import com.smartparking.service.FavoriteService;
+import com.smartparking.service.ParkingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ParkingServiceImpl extends AbstractService<Favorite, Long, FavoriteRepository> implements FavoriteService {
+public class ParkingServiceImpl extends AbstractService<Parking, Long, ParkingRepository> implements ParkingService {
 
-    protected ParkingServiceImpl(@Autowired FavoriteRepository repository) {
+    protected ParkingServiceImpl(@Autowired ParkingRepository repository) {
         super(repository);
     }
 }
