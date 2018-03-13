f29342679f26d71fe68c38ff5db1c66ce2b88833
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-03-13 17:03:46 +0200

    Fixed SpotServiceImpl.

--- persistence/src/main/java/com/smartparking/service/impl/SpotServiceImpl.java
+++ persistence/src/main/java/com/smartparking/service/impl/SpotServiceImpl.java
@@ -1,16 +1,16 @@
 package com.smartparking.service.impl;
 
-import com.smartparking.entity.Provider;
-import com.smartparking.repository.ProviderRepository;
+import com.smartparking.entity.Spot;
+import com.smartparking.repository.SpotRepository;
 import com.smartparking.service.AbstractService;
-import com.smartparking.service.ProviderService;
+import com.smartparking.service.SpotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SpotServiceImpl extends AbstractService<Provider, Long, ProviderRepository> implements ProviderService {
+public class SpotServiceImpl extends AbstractService<Spot, Long, SpotRepository> implements SpotService {
 
-    protected SpotServiceImpl(@Autowired ProviderRepository repository) {
+    protected SpotServiceImpl(@Autowired SpotRepository repository) {
         super(repository);
     }
 }
