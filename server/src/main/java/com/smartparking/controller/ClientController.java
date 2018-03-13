9dfb75edd5aa57a76616bc733eb813d54b32a11a
shoomi <andriy.shemechko@gmail.com>
2018-03-13 22:13:20 +0200

    edit client controller

--- server/src/main/java/com/smartparking/controller/ClientController.java
+++ server/src/main/java/com/smartparking/controller/ClientController.java
@@ -16,7 +16,7 @@ public class ClientController {
     ClientService clientService;
 
     @CrossOrigin(origins = "http://localhost:4200")
-    @GetMapping("providers")
+    @GetMapping("client")
     List<Client> findAll() {
         return clientService.findAll();
     }
