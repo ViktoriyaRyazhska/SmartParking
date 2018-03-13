53475220697b068367f0c2c20d8bf69a0efa163e
shoomi <andriy.shemechko@gmail.com>
2018-03-13 19:02:39 +0200

    added ClientDto, ClientController, client.service.ts,path 'configuration/clientlist' to app-routing.modules

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ParkingsComponent} from './parkings/parkings.component';
 import {SuperuserConfigurationComponent} from "./superuser-configuration/superuser-configuration.component";
+import {ClientsComponent} from "./clients/clients.component";
 
 const routes: Routes = [
     {path: '', redirectTo: '/index', pathMatch: 'full'},
     {path: 'index', component: ParkingsComponent},
-    {path: 'configuration', component: SuperuserConfigurationComponent, }
+    {path: 'configuration', component: SuperuserConfigurationComponent,},
+    {path: 'configuration/clientlist', component: ClientsComponent}
 ];
 
 @NgModule({
--- server/src/main/java/com/smartparking/dto/ClientDto.java
+++ server/src/main/java/com/smartparking/dto/ClientDto.java
@@ -1,4 +1,70 @@
 package com.smartparking.dto;
 
+import java.util.List;
+
 public class ClientDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private List<FavoriteDto> favoriteDto;
+    private ProviderDto providerDto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<FavoriteDto> getFavoriteDto() {
+        return favoriteDto;
+    }
+
+    public void setFavoriteDto(List<FavoriteDto> favoriteDto) {
+        this.favoriteDto = favoriteDto;
+    }
+
+    public ProviderDto getProviderDto() {
+        return providerDto;
+    }
+
+    public void setProviderDto(ProviderDto providerDto) {
+        this.providerDto = providerDto;
+    }
 }
