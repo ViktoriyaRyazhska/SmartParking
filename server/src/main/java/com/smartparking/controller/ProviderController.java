8379b43ce683ab034689d5c0ef643ab2e7c7b16a
Yura Katola <yurakatola@gmail.com>
2018-03-13 20:52:58 +0200

    Changed returned type from Provider to ProviderDto in ProviderController method.

--- server/src/main/java/com/smartparking/controller/ProviderController.java
+++ server/src/main/java/com/smartparking/controller/ProviderController.java
@@ -1,5 +1,7 @@
 package com.smartparking.controller;
 
+import com.smartparking.dto.AddressDto;
+import com.smartparking.dto.ProviderDto;
 import com.smartparking.entity.Provider;
 import com.smartparking.service.ProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,8 +20,24 @@ public class ProviderController {
 
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("providers")
-    List<Provider> findAll() {
-        return providerService.findAll();
+    List<ProviderDto> findAll() {
+        List<Provider> providers = providerService.findAll();
+        List<ProviderDto> dtoProviders = new ArrayList<>();
+        AddressDto addressDto;
+        ProviderDto providerDto;
+        for (Provider provider : providers) {
+            addressDto = AddressDto.builder()
+                    .setCity(provider.getLegalAddress().getCity())
+                    .setStreet(provider.getLegalAddress().getStreet())
+                    .setBuildingNumber(provider.getLegalAddress().getBuildingNumber());
+            providerDto = ProviderDto.builder()
+                    .setName(provider.getName())
+                    .setId(provider.getId())
+                    .setLegalAddressDto(addressDto);
+            dtoProviders.add(providerDto);
+        }
+        return dtoProviders;
     }
-
 }
+
+
