dd7c49424306aa029a4370853d4388fb46d80212
Yura Katola <yurakatola@gmail.com>
2018-03-13 16:19:38 +0200

    Added route for supruser-configuration.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {ParkingsComponent} from './parkings/parkings.component';
+import {SuperuserConfigurationComponent} from "./superuser-configuration/superuser-configuration.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/index', pathMatch: 'full' },
-  {path: 'index', component: ParkingsComponent}
+    {path: '', redirectTo: '/index', pathMatch: 'full'},
+    {path: 'index', component: ParkingsComponent},
+    {path: 'configuration', component: SuperuserConfigurationComponent, }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,40 +1,44 @@
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule }    from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
 
 
+import {AppComponent} from './app.component';
+import {AppNavbarHeaderComponent} from './app-navbar-header/app-navbar-header.component';
+import {ParkingsComponent} from './parkings/parkings.component';
+import {ParkingService} from './parking.service';
+import {AppNavbarFooterComponent} from './app-navbar-footer/app-navbar-footer.component';
+import {GeoLocationService} from './geo-location.service';
 
-import { AppComponent } from './app.component';
-import { AppNavbarHeaderComponent } from './app-navbar-header/app-navbar-header.component';
-import { ParkingsComponent } from './parkings/parkings.component';
-import { ParkingService } from './parking.service';
-import { AppNavbarFooterComponent } from './app-navbar-footer/app-navbar-footer.component';
-import { GeoLocationService } from './geo-location.service';
 
-
-import { AppRoutingModule } from './/app-routing.module';
+import {AppRoutingModule} from './/app-routing.module';
+import {SuperuserConfigurationComponent} from './superuser-configuration/superuser-configuration.component';
+import {ProviderService} from "./provider.service";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AppNavbarHeaderComponent,
-    ParkingsComponent,
-    AppNavbarFooterComponent
-  ],
-  imports: [
-    HttpClientModule,
-    BrowserModule,
-    RouterModule,
-    NgbModule.forRoot(),
-    AppRoutingModule
-  ],
-  providers: [
-    ParkingService,
-    GeoLocationService,
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AppNavbarHeaderComponent,
+        ParkingsComponent,
+        AppNavbarFooterComponent,
+        SuperuserConfigurationComponent
+    ],
+    imports: [
+        HttpClientModule,
+        BrowserModule,
+        RouterModule,
+        NgbModule.forRoot(),
+        AppRoutingModule
+    ],
+    providers: [
+        ParkingService,
+        GeoLocationService,
+        ProviderService
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
