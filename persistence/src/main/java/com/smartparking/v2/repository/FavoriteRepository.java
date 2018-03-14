6aa3a2fd72fa149eca9a8ed2cbd5bb48b27e8035
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-03-14 13:28:31 +0200

    Deleted unused package v2.repository.

--- persistence/src/main/java/com/smartparking/v2/repository/FavoriteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smartparking.v2.repository;
-
-import com.smartparking.entity.Favorite;
-
-public interface FavoriteRepository extends Repository<Favorite, Long> {
-
-}
--- persistence/src/main/java/com/smartparking/v2/repository/SpotRepository.java
+++
@@ -1,7 +0,0 @@
-package com.smartparking.v2.repository;
-
-import com.smartparking.entity.Spot;
-
-public interface SpotRepository extends Repository<Spot, Long> {
-
-}
