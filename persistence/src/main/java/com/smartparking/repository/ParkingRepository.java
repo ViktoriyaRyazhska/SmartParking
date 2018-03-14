6fad60542d01cdeb7e52c98594e16b77f6b86303
Yura Katola <yurakatola@gmail.com>
2018-03-14 22:33:04 +0200

    Added method findAllByProviderId on all datatransfer layers. And added ProviderDetailResponse, ProviderItemResponse classess.

--- persistence/src/main/java/com/smartparking/repository/ParkingRepository.java
+++ persistence/src/main/java/com/smartparking/repository/ParkingRepository.java
@@ -2,6 +2,9 @@ package com.smartparking.repository;
 
 import com.smartparking.entity.Parking;
 
+import java.util.List;
+
 public interface ParkingRepository extends Repository<Parking, Long> {
 
+    List<Parking> findAllByProviderId(Long id);
 }
