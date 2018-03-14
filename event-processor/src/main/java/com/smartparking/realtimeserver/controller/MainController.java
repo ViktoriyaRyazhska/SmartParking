22422c9383616f7daf1ddbfa23973f3b141f6c3a
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-03-14 13:30:34 +0200

    Implements 'provider/update' controller method in event-processor.

--- event-processor/src/main/java/com/smartparking/realtimeserver/controller/MainController.java
+++ event-processor/src/main/java/com/smartparking/realtimeserver/controller/MainController.java
@@ -1,50 +1,53 @@
 package com.smartparking.realtimeserver.controller;
 
-import com.smartparking.entity.Address;
 import com.smartparking.entity.Provider;
-import com.smartparking.service.AddressService;
 import com.smartparking.service.ProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import javax.annotation.PostConstruct;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class MainController {
 
-    private List<Provider> providers;
+    private Map<Long, Provider> providers;
 
     @Autowired
     private ProviderService providerService;
 
-    @Autowired
-    private AddressService addressService;
-//
-//    @PostConstruct
-//    public void init() {
-//        providers = providerService.findAll();
-//    }
+    @PostConstruct
+    public void init() {
+        providers = providerService.findAll().stream()
+                .collect(Collectors.toMap(Provider::getId, p -> p));
+    }
 
     @RequestMapping("provider/update")
-    public ResponseEntity providerUpdated() {
-        Address address = new Address();
-        address.setState("Lvivska");
-        address.setCity("Lviv");
-        address.setStreet("Shevchenka");
-        address.setBuildingNumber("1");
-        addressService.save(address);
+    public ResponseEntity providerUpdated(@RequestParam String id) {
+        final Long providerId;
+        try {
+            providerId = Long.valueOf(id);
+        } catch (NumberFormatException ex) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
 
-        Provider provider = new Provider();
-        provider.setId(1L);
-        provider.setLegalAddress(address);
-        provider.setName("Provider");
-        provider.setActive(true);
-        providerService.save(provider);
-
-        providers = providerService.findAll();
-
-        return ResponseEntity.ok(null);
+        Provider provider = providers.get(providerId);
+        if (provider == null) {
+            provider = providerService.findById(providerId);
+            if (provider == null) {
+                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            } else {
+                providers.put(providerId, provider);
+                return new ResponseEntity(HttpStatus.OK);
+            }
+        } else {
+            providerService.refresh(provider);
+            return new ResponseEntity(HttpStatus.OK);
+        }
     }
 }
