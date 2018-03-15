c26ea163c222ca4407cfbc33fb1afcb530062532
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-03-15 19:27:51 +0200

    Removed unused repository implementations.

--- persistence/src/main/java/com/smartparking/repository/EventRepository.java
+++ persistence/src/main/java/com/smartparking/repository/EventRepository.java
@@ -1,8 +1,14 @@
 package com.smartparking.repository;
 
 import com.smartparking.entity.Event;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EventRepository extends Repository<Event, Long> {
-    Event findBySpotId(Long spotId);
 
+    @Query("SELECT event " +
+            "FROM Event event " +
+            "WHERE event.departureTime IS NULL AND " +
+            "event.spot.id = :spotId")
+    Event findBySpotId(@Param("spotId") Long spotId);
 }
