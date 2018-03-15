e53dd84199babce12ab0d64810fb9d1310411865
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-03-15 19:46:43 +0200

    Removed unused method 'refresh' in repositories.

--- persistence/src/main/java/com/smartparking/repository/EventRepository.java
+++ persistence/src/main/java/com/smartparking/repository/EventRepository.java
@@ -1,10 +1,11 @@
 package com.smartparking.repository;
 
 import com.smartparking.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface EventRepository extends Repository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, Long> {
 
     @Query("SELECT event " +
             "FROM Event event " +
