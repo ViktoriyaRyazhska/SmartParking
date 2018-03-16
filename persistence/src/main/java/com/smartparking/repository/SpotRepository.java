c9bc267f8b6f555e64651c8a74c247a4b5b9a749
bernevek <bernevek1995@ukr.net>
2018-03-16 21:01:48 +0200

    temporarily disabled spot services

--- persistence/src/main/java/com/smartparking/repository/SpotRepository.java
+++ persistence/src/main/java/com/smartparking/repository/SpotRepository.java
@@ -2,20 +2,19 @@ package com.smartparking.repository;
 
 import com.smartparking.entity.Spot;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface SpotRepository extends JpaRepository<Spot, Long> {
 
-    @Query("SELECT s FROM Parking p JOIN p.spots s JOIN s.events e WHERE p.id=?1 AND e.departureTime IS NOT NULL")
-    List<Spot> findAllAvailableSpotsByParkingId(Long id);
-
-    @Query("SELECT COUNT(s) FROM Parking p JOIN p.spots s JOIN s.events e WHERE p.id=?1 AND e.departureTime IS NOT NULL")
-    Long countAvailableSpotsByParkingId(Long id);
-
-    @Query("SELECT COUNT(s) FROM Parking p JOIN p.spots s WHERE p.id=?1")
-    Long countAllSpotsByParkingId(Long id);
+//    @Query("SELECT s FROM Parking p JOIN p.spots s JOIN s.events e WHERE p.id=?1 AND e.departureTime IS NOT NULL")
+//    List<Spot> findAllAvailableSpotsByParkingId(Long id);
+//
+//    @Query("SELECT COUNT(s) FROM Parking p JOIN p.spots s JOIN s.events e WHERE p.id=?1 AND e.departureTime IS NOT NULL")
+//    Long countAvailableSpotsByParkingId(Long id);
+//
+//    @Query("SELECT COUNT(s) FROM Parking p JOIN p.spots s WHERE p.id=?1")
+//    Long countAllSpotsByParkingId(Long id);
 
     List<Spot> findAllByParkingId(Long id);
 
