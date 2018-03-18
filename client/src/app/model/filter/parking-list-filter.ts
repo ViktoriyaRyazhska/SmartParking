936920421576ce1302e78c7eff561d038c9bdadb
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-03-18 18:47:39 +0200

    Renamed ParkingListFilterParameters class to ParkingListFilter.

---
+++ client/src/app/model/filter/parking-list-filter.ts
@@ -0,0 +1,8 @@
+export class ParkingListFilter {
+    city: string;
+    street: string;
+    buildingNumber: string;
+    public latitude: number;
+    public longitude: number;
+    public radius: number;
+}
