c2fd42c5f98268b7a3e3378f7d2064be464f9da5
Yura Katola <yurakatola@gmail.com>
2018-03-18 12:49:53 +0200

    Added filtering to method getProviders(..) by ProviderListFilterParameters class (Currently only by state)

---
+++ client/src/app/model/filter/provider-list-filetr-parameters.ts
@@ -0,0 +1,3 @@
+export class ProviderListFilterParameters {
+    active: string;
+}
--- client/src/app/providers/provider-list/provider-list.component.ts
+++ client/src/app/providers/provider-list/provider-list.component.ts
@@ -1,23 +1,33 @@
 import {Component, OnInit} from '@angular/core';
-import {Provider} from "../provider";
-import {ProviderService} from "../provider.service";
+import {Provider} from '../provider';
+import {ProviderService} from '../provider.service';
+import {ProviderListFilterParameters} from '../../model/filter/provider-list-filetr-parameters';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-provider-list',
-  templateUrl: './provider-list.component.html',
-  styleUrls: ['./provider-list.component.css']
+    selector: 'app-provider-list',
+    templateUrl: './provider-list.component.html',
+    styleUrls: ['./provider-list.component.css']
 })
 export class ProviderListComponent implements OnInit {
     providers: Provider[];
+    providerFilterForm = new FormGroup({
+        active: new FormControl('null', [])});
+    providerFilter: ProviderListFilterParameters;
 
-    constructor(private providerService: ProviderService) { }
+    constructor(private providerService: ProviderService,
+                private formBuilder: FormBuilder) {
+    }
 
     ngOnInit() {
         this.getProviders();
     }
 
     getProviders(): void {
-        this.providerService.getAll()
+        this.providerFilter = this.providerFilterForm.value;
+        console.log(this.providerFilter);
+        this.providerService.getProviders(this.providerFilter)
             .subscribe(providers => this.providers = providers);
     }
+
 }
