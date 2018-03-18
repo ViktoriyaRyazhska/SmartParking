75bdb360f366df9cf1a57fed20036d55b51b6b53
Yura Katola <yurakatola@gmail.com>
2018-03-18 12:45:58 +0200

    Added directory model.filter to persistence module and class ProviderFilter.

---
+++ persistence/src/main/java/com/smartparking/model/filter/ProviderFilter.java
@@ -0,0 +1,14 @@
+package com.smartparking.model.filter;
+
+public class ProviderFilter {
+
+    private String active;
+
+    public String getActive() {
+        return active;
+    }
+
+    public void setActive(String active) {
+        this.active = active;
+    }
+}
