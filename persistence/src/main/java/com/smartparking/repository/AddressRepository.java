9be3d02466eb197c22d0e36d84882eb8a6b230e4
Yura Katola <yurakatola@gmail.com>
2018-03-20 17:57:50 +0200

    Added ProviderFilterRepository and ProviderRepositoryImpl. Relocate filtering to ProviderRepositoryImpl.

--- persistence/src/main/java/com/smartparking/repository/AddressRepository.java
+++ persistence/src/main/java/com/smartparking/repository/AddressRepository.java
@@ -1,7 +1,10 @@
 package com.smartparking.repository;
 
 import com.smartparking.entity.Address;
+import com.smartparking.entity.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
