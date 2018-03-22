e2ec99b5a9b27f17b631212942e80bed98ffbeae
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-03-22 21:37:24 +0200

    Added all data in ParkingResponse.

--- server/src/main/java/com/smartparking/controller/ParkingController.java
+++ server/src/main/java/com/smartparking/controller/ParkingController.java
@@ -5,15 +5,12 @@ import com.smartparking.entity.Parking;
 import com.smartparking.model.request.ParkingRequest;
 import com.smartparking.model.response.ManagerParkingResponse;
 import com.smartparking.model.response.ParkingDetailResponse;
-import com.smartparking.model.response.ParkingItemResponse;
 import com.smartparking.service.ParkingService;
 import com.smartparking.service.SpotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class ParkingController {
@@ -35,9 +32,7 @@ public class ParkingController {
         if (radius < 0) {
             return new ResponseEntity<>("Radius must be positive or zero.", HttpStatus.BAD_REQUEST);
         }
-        List<ParkingItemResponse> parkingItems =
-                ParkingItemResponse.listOf(parkingService.findAllNearby(latitude, longitude, radius));
-        return new ResponseEntity<>(parkingItems, HttpStatus.OK);
+        return new ResponseEntity<>(parkingService.findAllNearby(latitude, longitude, radius), HttpStatus.OK);
     }
 
     @RequestMapping("parkingdetail/{id}")
