8a0c4920075aa8ff98ca2b6bf9fe9b8d59498ea5
KrOst <20littlejohnny16@gmail.com>
2018-03-22 15:20:23 +0200

    fix conflicts

--- client/src/app/angular-materials.module.ts
+++ client/src/app/angular-materials.module.ts
@@ -12,45 +12,11 @@ import {
     MatTabsModule,
     MatToolbarModule,
 } from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatTabsModule,
-    MatSelectModule
-} from '@angular/material';
 
 
 @NgModule({
     imports: [
         BrowserAnimationsModule,
-        NoopAnimationsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatIconModule,
-        MatButtonModule,
-        MatToolbarModule,
-        MatTabsModule,
-        MatSelectModule
-    ],
-    exports: [
-        BrowserAnimationsModule,
-        NoopAnimationsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatIconModule,
-        MatButtonModule,
-        MatToolbarModule,
-        MatTabsModule,
-        MatSelectModule
-    ]
-    imports: [
-        BrowserAnimationsModule,
-        // NoopAnimationsModule,
         MatFormFieldModule,
         MatInputModule,
         MatIconModule,
@@ -63,7 +29,6 @@ import {
     ],
     exports: [
         BrowserAnimationsModule,
-        // NoopAnimationsModule,
         MatFormFieldModule,
         MatInputModule,
         MatIconModule,
