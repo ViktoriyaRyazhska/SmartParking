07c91d8481c610254b667a2ea40f0b65bb73196d
Yura Katola <yurakatola@gmail.com>
2018-03-28 17:26:40 +0300

    Fixed bug that doesn't allow to attend another websites.

--- client/src/app/interceptor.service.ts
+++ client/src/app/interceptor.service.ts
@@ -1,41 +1,32 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
     HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest,
     HttpResponse
-} from "@angular/common/http";
-import {Observable} from "rxjs/Observable";
+} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
-import {TokenStorage} from "./auth/token/token-storage";
-import {Router} from "@angular/router";
+import {TokenStorage} from './auth/token/token-storage';
+import {Router} from '@angular/router';
 
 @Injectable()
-export class InterceptorService implements HttpInterceptor{
+export class InterceptorService implements HttpInterceptor {
 
-  constructor(private router: Router) { }
+    constructor(private router: Router) {
+    }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      if(TokenStorage.getToken() != null) {
-          request = request.clone({
-              headers: request.headers.append('Authorization', `Bearer ${TokenStorage.getToken()}`)
-          });
-          return next.handle(request).do((event: HttpEvent<any>) => {
-              if (event instanceof HttpResponse) {
-              }
-          }, (err: any) => {
-              if (err instanceof HttpErrorResponse) {
-                  if (err.status === 401) {
-                      this.router.navigate(['/login'])
-                  }
-              }
-          });
-      }
+        if (request.url.startsWith('http://localhost:8080/') && (TokenStorage.getToken())) {
+            request = request.clone({
+                headers: request.headers.append('Authorization', `Bearer ${TokenStorage.getToken()}`)
+            });
+        }
         return next.handle(request).do((event: HttpEvent<any>) => {
             if (event instanceof HttpResponse) {
             }
         }, (err: any) => {
             if (err instanceof HttpErrorResponse) {
                 if (err.status === 401) {
-                    this.router.navigate(['/login'])
+                    this.router.navigate(['/login']);
                 }
             }
         });
