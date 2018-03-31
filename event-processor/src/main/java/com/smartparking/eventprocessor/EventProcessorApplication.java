0d4acc972c3e95ff57349548bdd16af356630cb9
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-03-31 12:29:47 +0300

    Renamed main package in event-processor.

--- event-processor/src/main/java/com/smartparking/eventprocessor/EventProcessorApplication.java
+++ event-processor/src/main/java/com/smartparking/eventprocessor/EventProcessorApplication.java
@@ -1,4 +1,4 @@
-package com.smartparking;
+package com.smartparking.eventprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- event-processor/src/main/java/com/smartparking/eventprocessor/element/InRequest.java
+++ event-processor/src/main/java/com/smartparking/eventprocessor/element/InRequest.java
@@ -1,8 +1,8 @@
-package com.smartparking.element;
+package com.smartparking.eventprocessor.element;
 
-import com.smartparking.controller.MainController;
 import com.smartparking.entity.Event;
 import com.smartparking.entity.EventMarker;
+import com.smartparking.eventprocessor.controller.MainController;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class InRequest {
