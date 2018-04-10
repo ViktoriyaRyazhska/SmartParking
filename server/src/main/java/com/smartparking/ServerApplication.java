00b1b4da6649013ea9473a3388b9062c345f16bf
Vadym <t.vadyk@gmail.com>
2018-04-10 14:08:04 +0300

    Fix ServerApplication and add properties

--- server/src/main/java/com/smartparking/ServerApplication.java
+++ server/src/main/java/com/smartparking/ServerApplication.java
@@ -20,12 +20,5 @@ public class ServerApplication {
     public static void main(String[] args) throws IOException {
         SpringApplication.run(ServerApplication.class, args);
         System.out.println("EXIT AND CLOSE fsdgahsjdgasjhdgf");
-        CloseableHttpClient client = HttpClients.createDefault();
-        for (; ; ) {
-            HttpPost post = new HttpPost("http://localhost:8081/spot/update");
-            post.setEntity(new StringEntity("{\"spotId\":1,\"parkingToken\":\"1234\",\"eventType\":\"ARRIVE\",\"timestamp\":1}"));
-            CloseableHttpResponse execute = client.execute(post);
-            System.out.println(execute);
-        }
     }
 }
