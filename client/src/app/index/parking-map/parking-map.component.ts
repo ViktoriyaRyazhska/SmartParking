28dbe4a48cf2a7bfa2b1348f40fc32dcb6c7309a
Yura Katola <yurakatola@gmail.com>
2018-04-11 16:21:12 +0300

    Added method for converting and suit form representation view from mile to kilometers.

--- client/src/app/index/parking-map/parking-map.component.ts
+++ client/src/app/index/parking-map/parking-map.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {Parking} from '../../model/view/parking';
+import {Browser} from 'leaflet';
+
+const MiToKm = 1.60934;
 
 @Component({
     selector: 'app-parking-map',
@@ -7,11 +10,13 @@ import {Parking} from '../../model/view/parking';
     styleUrls: ['./parking-map.component.css']
 })
 
+
 export class ParkingMapComponent implements OnInit {
     lat: number;
     lng: number;
     parkings: Parking[];
     dir = undefined;
+    distance: string;
 
     constructor() {
     }
@@ -23,11 +28,23 @@ export class ParkingMapComponent implements OnInit {
     getDirection(lat, lng) {
         this.dir = {
             origin: {lat: this.lat, lng: this.lng},
-            destination: {lat: lat, lng: lng}
+            destination: {lat: lat, lng: lng},
+            drivingOptions: {
+                departureTime: new Date(Date.now()),
+                trafficModel: 'pessimistic'
+            },
+            travelMode: 'DRIVING'
         };
     }
 
     round(floatNumber: number): number {
         return Math.floor(floatNumber) / 1000;
+    }
+
+    revertMileToKm(distance: any): number {
+        distance = distance.substr(0, distance.indexOf(' '));
+        Number.parseInt(distance);
+        distance *= MiToKm;
+        return Math.floor(distance * 10) / 10;
     }
 }
