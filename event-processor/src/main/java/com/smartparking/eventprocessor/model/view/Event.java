00694748d964f4c401e9cb790c947934f769e3a6
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-04-13 01:29:08 +0300

    Added saving events directly to database using JdbcTemplate.

--- event-processor/src/main/java/com/smartparking/eventprocessor/model/view/Event.java
+++ event-processor/src/main/java/com/smartparking/eventprocessor/model/view/Event.java
@@ -11,6 +11,7 @@ import java.time.Instant;
 @NoArgsConstructor
 @AllArgsConstructor
 public abstract class Event {
+
     private Long spotId;
     private EventType eventType;
     private Instant timestamp;
