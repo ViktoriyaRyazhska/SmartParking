a5b0fd213167d26c3e23225dea24ab0829db4f8e
shoomi <andriy.shemechko@gmail.com>
2018-04-12 23:00:51 +0300

    deleted unused imports in ClientRepository, SpotRepository, ClientServiceImpl, SpotService

--- server/src/main/java/com/smartparking/repository/SpotRepository.java
+++ server/src/main/java/com/smartparking/repository/SpotRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.Instant;
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 public interface SpotRepository extends JpaRepository<Spot, Long> {
