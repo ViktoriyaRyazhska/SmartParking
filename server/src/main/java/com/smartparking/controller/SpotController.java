7eb5bb61161b8a81f5cd97d9cf8fa4e7d57fb50e
Vadym <t.vadyk@gmail.com>
2018-04-17 13:44:34 +0300

    Make social icons on login page Make validation on favorite name input, <= 25 chars

--- server/src/main/java/com/smartparking/controller/SpotController.java
+++ server/src/main/java/com/smartparking/controller/SpotController.java
@@ -67,7 +67,8 @@ public class SpotController {
             @RequestParam("start_time") String startTime,
             @RequestParam("end_time") String endTime) {
 
-        List<SpotStatisticResponse> spotStatisticResponseList = spotService.getSpotStatistic(id,Long.parseLong(startTime),Long.parseLong(endTime));
+        List<SpotStatisticResponse> spotStatisticResponseList =
+                spotService.getSpotStatistic(id,Long.parseLong(startTime),Long.parseLong(endTime));
         return new ResponseEntity<>(spotStatisticResponseList, HttpStatus.OK);
     }
 
