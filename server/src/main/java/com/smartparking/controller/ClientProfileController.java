e63090d39c9199a22e95e3b529ba2a93b45dc028
Vadym <t.vadyk@gmail.com>
2018-04-19 12:44:52 +0300

    Code refactor

--- server/src/main/java/com/smartparking/controller/ClientProfileController.java
+++ server/src/main/java/com/smartparking/controller/ClientProfileController.java
@@ -1,65 +1,29 @@
 package com.smartparking.controller;
 
 import com.smartparking.entity.Client;
-import com.smartparking.entity.PasswordConfirmation;
 import com.smartparking.model.request.ClientRequest;
-import com.smartparking.model.request.PasswordRequest;
 import com.smartparking.model.response.ClientDetailResponse;
-import com.smartparking.model.response.InfoResponse;
 import com.smartparking.model.response.ParkingDetailResponse;
-import com.smartparking.security.exception.AuthorizationEx;
 import com.smartparking.service.ClientService;
 import com.smartparking.service.FavoriteService;
-import com.smartparking.service.ParkingService;
-import com.smartparking.service.PasswordConfirmationService;
-import com.smartparking.service.email.EmailService;
-import com.smartparking.service.impl.SecurityServiceImpl;
-import com.smartparking.service.impl.ExpirationCheckService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.MailException;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/profile")
 public class ClientProfileController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ClientProfileController.class);
 
     @Autowired
     ClientService clientService;
 
     @Autowired
     FavoriteService favoriteService;
-
-    @Autowired
-    ParkingService parkingService;
-
-    @Autowired
-    SecurityServiceImpl securityServiceImpl;
-
-    @Autowired
-    EmailService emailService;
-
-    @Autowired
-    private PasswordEncoder bcryptEncoder;
-
-    @Autowired
-    private PasswordConfirmationService passwordConfirmationService;
-
-    @Autowired
-    private ExpirationCheckService expirationCheckService;
 
     @Value("${cross_origin.client}")
     String hostUrl;
@@ -81,37 +45,6 @@ public class ClientProfileController {
         } else {
             return new ResponseEntity<>("Empty data input.", HttpStatus.NO_CONTENT);
         }
-    }
-
-    @PostMapping(value = "/update/password")
-    public ResponseEntity saveUser(@RequestBody String uuidFromUrl) {
-        PasswordConfirmation checkedPasswordConfirmation =
-                expirationCheckService.getPasswordConfirmationWithExpirationChecking(uuidFromUrl);
-
-        if (uuidFromUrl.equals(checkedPasswordConfirmation.getUuid())) {
-            securityServiceImpl.updateClientEncodedPassword(checkedPasswordConfirmation.getNewPassword());
-            passwordConfirmationService.delete(checkedPasswordConfirmation);
-            return ResponseEntity.status(HttpStatus.OK).body(new InfoResponse("You are successfully updated password"));
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new InfoResponse("Error during password changing"));
-        }
-    }
-
-    @PostMapping("/update/password/confirm")
-    public ResponseEntity sendConfirmation(@RequestBody PasswordRequest passwordRequest) {
-        final String uuid = UUID.randomUUID().toString().replace("-", "");
-        final String confirmUrl = hostUrl + "/update/password/" + uuid;
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        String firstName = clientService.findOne(email).getFirstName();
-        passwordConfirmationService.save(
-                passwordConfirmationService.makePasswordConfirmationEntity(uuid, passwordRequest.getPassword()));
-        try {
-            emailService.prepareAndSendConfirmPassEmail(email, firstName, confirmUrl);
-        } catch (MailException e) {
-            LOGGER.error("Could not send email to : {} Error = {}", email, e.getMessage());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email sending Error!");
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(new InfoResponse("Data saved successfully"));
     }
 
     @GetMapping(value = "/favorites")
