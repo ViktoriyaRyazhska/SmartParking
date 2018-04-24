4ae3a5e5f10a5b3f824a9ff7bd7680c12825ac32
Yura Katola <yurakatola@gmail.com>
2018-04-24 17:05:47 +0300

    Changed query that allows to get only parking which provider is active.

--- server/src/main/java/com/smartparking/repository/ParkingRepository.java
+++ server/src/main/java/com/smartparking/repository/ParkingRepository.java
@@ -12,11 +12,11 @@ public interface ParkingRepository extends JpaRepository<Parking, Long> {
 
     List<Parking> findAllByProviderId(Long id);
 
-//    List<String> findDistinctByCity();
 
-    @Query(value = "SELECT *, LOCATION_DISTANCE(:latitude, :longitude, parking.latitude, parking.longitude) AS distance " +
-            "FROM parking " +
-            "WHERE LOCATION_DISTANCE(:latitude, :longitude, parking.latitude, parking.longitude) <= :radius",
+    @Query(value = "SELECT parking.*, LOCATION_DISTANCE(:latitude, :longitude, parking.latitude, parking.longitude) AS distance " +
+            "FROM parking JOIN provider ON provider.id = parking.provider_id " +
+            "WHERE LOCATION_DISTANCE(:latitude, :longitude, parking.latitude, parking.longitude) <= :radius " +
+            "AND provider.active = TRUE",
             nativeQuery = true)
     List<Tuple> findAllNearby(@Param(value = "latitude") Double latitude,
                               @Param(value = "longitude") Double longitude,
