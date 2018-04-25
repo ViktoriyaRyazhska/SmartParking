8d67b03f2675c485211295499316d5f86e0c81b7
Vadym <t.vadyk@gmail.com>
2018-04-25 19:17:33 +0300

    Create scheduled method that clear temporaryDataConfirmation table Each Monday at midnight(00:00:00)

--- server/src/main/java/com/smartparking/ServerApplication.java
+++ server/src/main/java/com/smartparking/ServerApplication.java
@@ -3,12 +3,13 @@ package com.smartparking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableScheduling
 public class ServerApplication {
 
     public static void main(String[] args) throws IOException {
