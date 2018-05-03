849f6a4a1622cd10c810377c9862519b952a86fb
Yura Katola <yurakatola@gmail.com>
2018-05-03 21:44:01 +0300

    Final refactoring.

--- client/src/app/app-navbar-header/app-navbar-header.component.ts
+++ client/src/app/app-navbar-header/app-navbar-header.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {TokenStorage} from '../auth/token/token-storage';
-import {Role} from "../auth/roles";
-import {Router} from "@angular/router";
+import {Role} from '../auth/roles';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-navbar-header',
@@ -11,27 +11,27 @@ import {Router} from "@angular/router";
 export class AppNavbarHeaderComponent implements OnInit {
     role: any = Role;
 
-  navTitle = 'SmartParking';
+    navTitle = 'SmartParking';
 
     constructor(private router: Router,
                 private tokenStorage: TokenStorage) {
     }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  getRole(): Role {
-    return this.tokenStorage.getRole();
-  }
+    getRole(): Role {
+        return this.tokenStorage.getRole();
+    }
 
-  hasToken(): boolean{
-    return this.tokenStorage.hasToken();
-  }
+    hasToken(): boolean {
+        return this.tokenStorage.hasToken();
+    }
 
-  logOut() {
-      this.tokenStorage.signOut();
-      this.router.navigate(['index']);
-  }
+    logOut() {
+        this.tokenStorage.signOut();
+        this.router.navigate(['index']);
+    }
 
 
 }
